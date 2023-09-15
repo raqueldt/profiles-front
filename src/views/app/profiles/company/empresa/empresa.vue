05a039193ec5c8b94c5624195dbaf1bfad8d82a3
raqueldt <raquel.d@andeantc.com>
2023-09-15 15:53:26 -0500

    add vue-table-2 verison 1 listEmpresas

--- src/views/app/profiles/company/empresa/empresa.vue
+++ src/views/app/profiles/company/empresa/empresa.vue
@@ -1,18 +1,18 @@
 <template>
 	<div>
-		<TableComponent :data="rowdata" :columns="tableColumns" :options="options" />
-		<!-- {{ rowdata }} -->
+		<TableAdvanced :data="rowdata" :columns="tableColumns" :options="options" />
+		{{ rowdata }}
 	</div>
 </template>
 
 <script>
 import EmpresaServices from "../../../../../services/profiles/empresa/EmpresaServices";
-import TableComponent from '../../../../../components/UI/Tables/TableAdvanced.vue';
+import TableAdvanced from '../../../../../components/UI/Tables/TableAdvanced.vue';
 
 export default {
 	name: "empresa",
 	components: {
-		TableComponent
+		TableAdvanced
 	},
 	data() {
 		return {
@@ -22,21 +22,12 @@ export default {
 			options: {
 				perPage: 10,
 				perPageValues: [5, 10, 25, 50],
-				columns: {
-					name: {
-						label: 'Nombre Personalizado',
-					},
-					imagen: {
-						label: 'Imagen Personalizada',
-					},
-					// pertenece: {
-					// 	label: 'Pertenece a Personalizado',
-					// },
-					// pais: {
-					// 	label: 'País Personalizado',
-					// },
-				},
-				// filterable: ['name', 'imagen', 'pertenece', 'pais'],
+				columns: [
+					// 'id',
+					'name',
+
+				],
+				filterable: ['name'],
 				// css: {
 				// 	table: 'table table-striped table-bordered', // Agrega clases CSS a la tabla
 				// }
@@ -45,20 +36,13 @@ export default {
 	},
 	methods: {
 		async getAll() {
-			// EmpresaServices.getAll().then((response) => {
-			// 	this.rowdata = response.data.data;
-			// 	this.tableColumns = Object.keys(this.rowdata[0]).map(prop => ({ field: prop, label: prop }));
-			// 	console.log("aaa", this.rowdata)
-			// 	this.isLoadingRows = true;
-			// }).catch((error) => {
-			// 	console.log("errores" + error);
-			// }).finally(() => {
-			// });
+
 
 			try {
 				const response = await EmpresaServices.getAll()
 
 				this.rowdata = response.data.data;
+				this.tableColumns = Object.keys(this.rowdata[0]);
 				console.log(this.datos);
 			} catch (error) {
 				console.error("Error:", error);
